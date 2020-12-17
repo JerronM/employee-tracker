7138ecf145abe949aaf258082bab2a6bed8439f2
Jerron <jerron.mcginnis@gmail.com>
2020-12-17 16:39:42 -0500

    Added cli menu using inquirer

--- index.js
+++ index.js
@@ -36,3 +36,43 @@ const db = new Database({
     password: "",
     database: "employee_db"
 });
+
+//inteface
+function runApp() {
+    inquirer.prompt({
+        name: "mainmenu",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "Edit Employeee Info",
+            "View Roles",
+            "Edit Roles",
+            "View Departments",
+            "Edit Departments"
+        ]
+    }).then(responses => {
+        switch (responses.mainmenu) {
+            case "View All Employees":
+                showEmployeeSummary();
+                break;
+            case "Edit Employeee Info":
+                editEmployeeOptions();
+                break;
+            case "View Roles":
+                showRoleSummary();
+                break;
+            case "Edit Roles":
+                editRoleOptions();
+                break;
+            case "View Departments":
+                showDepartments();
+                break;
+            case "Edit Departments":
+                editDepartmentOptions();
+                break;
+        }
+    });
+}
+
+
